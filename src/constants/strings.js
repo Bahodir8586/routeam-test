8d04126ae3b601d489bcbe1d173a0e4573c61f9f
Abdullayev Bahodir <60338653+Bahodir8586@users.noreply.github.com>
2021-04-02 19:18:55 +0500

    Add files via upload

---
+++ src/constants/strings.js
@@ -0,0 +1,10 @@
+const Strings = {
+  errorMessageDefault: "Что-то пошло не так. Пожалуйста, попробуйте еще раз",
+  incorrectCode: "Введен неверный код. Пожалуйста, попробуйте еще раз",
+  incorrectFile: "Пожалуйста, загрузите видео",
+  submit: "Отправить",
+  enterCode: "Пожалуйста, введите код",
+  chooseFile: "Пожалуйста, выберите файл",
+};
+
+export default Strings;
